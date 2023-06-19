b9c002a018739b4648f87e8a21058284be8a08fc
zeroIsScared <69678222+zeroIsScared@users.noreply.github.com>
2023-06-19 10:48:55 +0300

    Update scrypt.js
    Added conditions

--- scrypt.js
+++ scrypt.js
@@ -1,3 +1,11 @@
+// JSON 1
+// The one and only task in this article concerns accessing JSON data and using it in your page. JSON data about some mother cats and their kittens is available in sample.json. The JSON is loaded into the page as a text string and made available in the catString parameter of the displayCatInfo() function. Your task is to fill in the missing parts of the displayCatInfo() function to store:
+
+// The names of the three mother cats, separated by commas, in the motherInfo variable.
+// The total number of kittens, and how many are male and female, in the kittenInfo variable.
+// The values of these variables are then printed to the screen inside paragraphs.
+//https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Test_your_skills:_JSON
+
 const section = document.querySelector('section');
 
 let para1 = document.createElement('p');
